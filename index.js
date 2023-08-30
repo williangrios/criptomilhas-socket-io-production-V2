3c0f2245f41d602cc5dc645ddf25a615d302076b
Willian Gonçalves Rios <71411461+williangrios@users.noreply.github.com>
2023-08-30 06:20:01 -0300

    fix: change port to 443

--- index.js
+++ index.js
@@ -86,6 +86,6 @@ io.on("connection", (socket) => {
 //   res.send("Server is running............");
 // });
 
-server.listen(8900, () => {
+server.listen(443, () => {
   console.log("SERVER RUNNING", server.address());
 });
