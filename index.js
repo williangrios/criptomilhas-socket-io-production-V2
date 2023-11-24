8a0279938dbd898ebac30fde29f45c67374ad194
Willian Gonçalves Rios <71411461+williangrios@users.noreply.github.com>
2023-11-24 15:23:03 -0300

    fix: update code

--- index.js
+++ index.js
@@ -7,48 +7,49 @@ const cors = require("cors");
 const PORT = process.env.PORT || 3000
 
 const app = express();
+
 const server = http.createServer(app);
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST'],
-  optionsSuccessStatus: 204,
-}));
+// app.use(cors({
+//   origin: '*',
+//   methods: ['GET', 'POST'],
+//   optionsSuccessStatus: 204,
+// }));
 
-const io = socketio(server, {
-  cors: {
-    origin: '*',
-    methods: ['GET', 'POST']
-  }
-});
+// const io = socketio(server, {
+//   cors: {
+//     origin: '*',
+//     methods: ['GET', 'POST']
+//   }
+// });
 
-io.on("connection", (socket) => {
-  socket.on("addUser", (userId) => {
-    addUser(userId, socket.id);
-    io.emit("getUsers", users);
-  });
+// io.on("connection", (socket) => {
+//   socket.on("addUser", (userId) => {
+//     addUser(userId, socket.id);
+//     io.emit("getUsers", users);
+//   });
 
-  socket.on(
-    "sendMessage",
-    ({ conversationId, senderId, receiverId, text, attachedImage, personalizedProposalId }) => {
-      const user = getUser(receiverId);
-      if (!user ) {
-        return
-      }
-      io.to(user?.socketId).emit("getMessage", {
-        conversationId,
-        senderId,
-        text,
-        attachedImage,
-        personalizedProposalId
-      });
-    }
-  );
+//   socket.on(
+//     "sendMessage",
+//     ({ conversationId, senderId, receiverId, text, attachedImage, personalizedProposalId }) => {
+//       const user = getUser(receiverId);
+//       if (!user ) {
+//         return
+//       }
+//       io.to(user?.socketId).emit("getMessage", {
+//         conversationId,
+//         senderId,
+//         text,
+//         attachedImage,
+//         personalizedProposalId
+//       });
+//     }
+//   );
 
-  socket.on("disconnect", () => {
-    removeUserBySocketId(socket.id)
-    io.emit("getUsers", users);
-  });
-});
+//   socket.on("disconnect", () => {
+//     removeUserBySocketId(socket.id)
+//     io.emit("getUsers", users);
+//   });
+// });
 
 server.listen(PORT, () => {
   console.log("SERVER RUNNING:", server.address(), ' PORT:', PORT);
