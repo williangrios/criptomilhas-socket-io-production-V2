1fbfd8715fe47e7cfba99b643de7fe112d96190f
Willian Gonçalves Rios <71411461+williangrios@users.noreply.github.com>
2023-11-24 11:40:18 -0300

    refactor: simple refactor

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const users = require('./users.js')
 router.get("/", (req, res) => {
-  res.send({ response: "Server is up", users }).status(200);
+  res.send({ response: "Server is up...", users }).status(200);
 });
 
 module.exports = router;
