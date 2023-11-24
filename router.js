14fcd387ffedf0120960863cba3b122a7a1d47a5
Willian Gonçalves Rios <71411461+williangrios@users.noreply.github.com>
2023-11-24 15:37:39 -0300

    fix: bugs

--- router.js
+++ router.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const users = require('./users.js')
+const cors = require("cors");
+router.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST'],
+  optionsSuccessStatus: 204,
+}))
 router.get("/", (req, res) => {
   res.send({ response: "Server is up", users }).status(200);
 });
